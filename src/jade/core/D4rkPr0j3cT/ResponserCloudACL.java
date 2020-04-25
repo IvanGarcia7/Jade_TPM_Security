410c108b702ad0d26524ec4b71d7caad75459a8d
IvanGarcia7 <ivanbolero@hotmail.com>
2020-04-25 23:43:24 +0200

    feat: Added some lines of code

--- src/jade/core/D4rkPr0j3cT/ResponserCloudACL.java
+++ src/jade/core/D4rkPr0j3cT/ResponserCloudACL.java
@@ -2,6 +2,8 @@ package jade.core.D4rkPr0j3cT;
 
 import jade.core.Agent;
 import jade.core.BaseService;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.lang.acl.ACLMessage;
 import jade.lang.acl.MessageTemplate;
 import jade.proto.SimpleAchieveREResponder;
 import jade.util.Logger;
@@ -15,5 +17,30 @@ public class ResponserCloudACL extends SimpleAchieveREResponder {
     public ResponserCloudACL(Agent ams, MessageTemplate mt, SecureCloudTPMService secureCloudTPMService) {
         super(ams, mt);
         myService = secureCloudTPMService;
+    }
+
+    /**
+     * THIS FUNCTION TAKE A ACL REQUEST, AND PROCESS IT ATTEND TO THE TYPE OF THE REQUEST
+     * @param request the received message
+     * @return
+     */
+    protected ACLMessage prepareResponse(ACLMessage request) {
+        System.out.println("PROCESSING THE REQUEST ATTESTATION IN THE DESTINY");
+        ACLMessage reply = request.createReply();
+        return reply;
+    }
+
+    protected ACLMessage prepareResultNotification(ACLMessage request, ACLMessage response) throws FailureException {
+        System.out.println("PREPARING THE RESULT NOTIFICATION");
+        ACLMessage reply = request.createReply();
+        return reply;
+    }
+
+    /**
+     * THIS FUNCTION PROCEED WITH THE SECURE MOVE OR CLONATION, IF THE MAXIMUM WAITING TIME IS NOT EXPIRED.
+     * @param inform
+     */
+    protected void handleInform(ACLMessage inform){
+        System.out.println("CATCH THE ACL MESSAGE IN THE HANDLE INFORM");
     }
 }
